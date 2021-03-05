031556976b21c315da60c3a42bcebd64bea15a45
chiboycalix <igwechinonso77@gmail.com>
2021-03-05 20:49:21 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+console.log(`
+Igwe Chinonso 
+
+I'm a Fullstack Javascript Engineer and Tech writer with over 3 years of experience building large scale web applications for both small and medium sized companies. I specialize in building efficient fullstack web applications that work across multiple platform and browsers and that are usable and pleasant for the most number of people possible. I use React, Typescript, GraphQL, Javascript, Node.js, SCSS and HTML.
+
+🐦 Twitter: https://twitter.com/thorsgardian_
+📖 GitHub: https://github.com/chiboycalix
+😎 Blog: https://dev.to/cigwe416
+💼 Portfolio: https://chiboycalix.github.io/chinonso.dev/
+
+`);
+
