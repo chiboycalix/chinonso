dcc9e0f20457f31e098b0b49ea900aa8c0bfd012
chiboycalix <igwechinonso77@gmail.com>
2021-03-06 09:58:20 +0100

    added colors

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 #!/usr/bin/env node
-const clearConsole = require('clear-any-console')
-clearConsole()
+const pkgJson = require('./package.json');
+const welcome = require('cli-welcome')
+
+welcome({
+  title: pkgJson.name,
+  tagLine: `Howdy, nice to meet you`,
+  description: pkgJson.description,
+  version: pkgJson.version,
+  bgColor: `#FADC00`,
+  color: `#000000`,
+  bold: true,
+  clear: true
+})
 console.log(`
 Igwe Chinonso 
 
